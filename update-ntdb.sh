6021ee745125263ba0d8b253d5b978d1ae2e9f4b
Remsnet LTD, <office@remsnet.de>
2013-12-28 21:33:54 +0100

    Update update-ntdb.sh

--- update-ntdb.sh
+++ update-ntdb.sh
@@ -1,3 +1,12 @@
-
 cd /srv/PROJ/samba4.1/sources/ntdb
 git clone git://git.debian.org/pkg-samba/ntdb.git
+cd ntdb
+cd git checkout
+cd /srv/PROJ/samba4.1/sources/ntdb
+if [ -d ntdb-1.0 ]; then
+rm -rf ntdb-1.0
+fi
+mv ntdb ntdb-1.0
+tar cpfz ntdb-1.0.tar.gz  ntdb-1.0
+cp -p ntdb-1.0.tar.gz /usr/src/packages/SOURCES
+
