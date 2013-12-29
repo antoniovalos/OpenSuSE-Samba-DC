78c5700c154d21bd0fb57792ee535ca3f721f69e
Remsnet LTD, <office@remsnet.de>
2013-12-29 11:28:54 +0100

    Create wget-hashlib.sh

---
+++ wget-hashlib.sh
@@ -0,0 +1,4 @@
+cd /srv/PROJ/samba4.1/sources
+wget http://code.krypto.org/python/hashlib/hashlib-20081119.tar.gz?attredirects=
+mv "hashlib-20081119.tar.gz\?attredirects\=0" hashlib-20081119.tar.gz
+cp hashlib-20081119.tar.gz /usr//src/packages/SOURCES/
