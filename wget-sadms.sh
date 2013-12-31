64e3356cf1a5e5f9d2a6c15ac440239f2b7b4586
Remsnet LTD, <office@remsnet.de>
2013-12-31 13:32:49 +0100

    Create wget-sadms.sh

---
+++ wget-sadms.sh
@@ -0,0 +1,4 @@
+cd /srv/PROJ/samba4.1/sources
+wget http://downloads.sourceforge.net/project/sadms/sadms2/sadms-2.0.15b/sadms-2.0.15b.tar.gz?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fsadms%2F&ts=1388492973&use_mirror=optimate
+mv "sadms-2.0.15b.tar.gz?r=http:%2F%2Fsourceforge.net%2Fprojects%2Fsadms%2F" sadms-2.0.15b.tar.gz
+cp sadms-2.0.15b.tar.gz /usr//src/packages/SOURCES/
