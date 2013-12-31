134a61d651cae35df94c093814afdf9c2f18412c
Remsnet LTD, <office@remsnet.de>
2013-12-31 12:16:02 +0100

    Create wget-dcpromo.sh

---
+++ wget-dcpromo.sh
@@ -0,0 +1,5 @@
+#
+# 
+# http://conradjonesit.wordpress.com/2013/06/24/building-a-samba4-domain-controller-on-opensuse-12-3/
+cd /srv/PROJ/samba4.1/sources
+wget http://downloads.sourceforge.net/project/bsdcontrol/screencast/samba4/config_samba4.tar.gz
